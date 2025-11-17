62dc5f7668802df88fb91dc32ada8d503a6d8fdb
MisterDodik <108690656+MisterDodik@users.noreply.github.com>
2025-11-17 23:30:12 +0100

    game loop ticker set up

--- Assets/Backend/internal/handlers/stargame.go
+++ Assets/Backend/internal/handlers/stargame.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"errors"
 	"log"
 
 	"github.com/MisterDodik/MultiplayerGame/internal/events"
@@ -9,6 +10,11 @@ import (
 
 func StartGameHandler(e events.Event, c *network.Client) error {
 	log.Println("game started")
+
+	if c.Lobby == nil {
+		return errors.New("client is not in lobby")
+	}
+	c.Lobby.StartGame()
 
 	network.BroadcastMessageToAllClients(c, &e) //sends an already existing start_game event to everyone
 	return nil

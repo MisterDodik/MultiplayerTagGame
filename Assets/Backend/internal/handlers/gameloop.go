3e3b710c7ac593706c7475d8dabec785774bcdf9
MisterDodik <108690656+MisterDodik@users.noreply.github.com>
2025-11-18 17:30:05 +0100

    Server side client movement

--- Assets/Backend/internal/handlers/gameloop.go
+++ Assets/Backend/internal/handlers/gameloop.go
@@ -2,12 +2,15 @@ package handlers
 
 import (
 	"encoding/json"
+	"math"
 
 	"github.com/MisterDodik/MultiplayerGame/internal/events"
 	"github.com/MisterDodik/MultiplayerGame/internal/network"
 )
 
 type PositionUpdatePayload struct {
+	InputX float64 `json:"inputX"`
+	InputY float64 `json:"inputY"`
 }
 
 func UpdatePositionHandler(e events.Event, c *network.Client) error {
@@ -16,5 +19,49 @@ func UpdatePositionHandler(e events.Event, c *network.Client) error {
 		return err
 	}
 
+	calculate_pos(c, payload.InputX, payload.InputY)
 	return nil
+}
+
+func calculate_pos(c *network.Client, inputX, inputY float64) {
+	newPosX := inputX * c.ClientGameData.Speed
+	newPosY := inputY * c.ClientGameData.Speed
+
+	tryX := c.ClientGameData.PosX + newPosX
+	tryY := c.ClientGameData.PosY + newPosY
+
+	if !collides(c, tryX, tryY) {
+		c.ClientGameData.PosX = tryX
+		c.ClientGameData.PosY = tryY
+		return
+	}
+
+	if !collides(c, tryX, c.ClientGameData.PosY) {
+		c.ClientGameData.PosX = tryX
+		return
+	}
+
+	if !collides(c, c.ClientGameData.PosX, tryY) {
+		c.ClientGameData.PosY = tryY
+		return
+	}
+}
+
+func collides(c *network.Client, x, y float64) bool {
+	for p := range c.Lobby.Clients {
+		if p == c {
+			continue
+		}
+
+		dx := x - p.ClientGameData.PosX
+		dy := y - p.ClientGameData.PosY
+
+		dist := math.Sqrt(dx*dx + dy*dy)
+
+		if dist < c.ClientGameData.Radius+p.ClientGameData.Radius {
+			return true
+		}
+	}
+
+	return false
 }

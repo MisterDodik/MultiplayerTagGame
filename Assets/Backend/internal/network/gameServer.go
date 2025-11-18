c56c1c2976df43e671cb08166393430eca1d51f8
MisterDodik <108690656+MisterDodik@users.noreply.github.com>
2025-11-18 17:31:39 +0100

    Removed comments

--- Assets/Backend/internal/network/gameServer.go
+++ Assets/Backend/internal/network/gameServer.go
@@ -36,41 +36,6 @@ func (gs *GameServer) StartGame(c *Client) {
 
 }
 
-/*
-client salje event u formatu:
-
-	{
-		type: updatePos
-		payload: {
-					input vector2
-				}
-	}
-
-onda se ovdje pozove funkcija koja for loopom prodje kroz sve igrace u lobiju
-calculate_pos(event.payload.input)
-
-broadcasttoeveryone(client.newpos)
-
-	func calculate_pos(input vector2){
-		newPosX = client.currentX + input.x * speed
-		newPosY = client.currentY + input.y * speed
-
-
-		//cekira collision
-		for p in players_in_lobby{
-			if p = client
-				continue
-			if rastojanje(p.posX, newPosX) < sizeof_client + sizeof_p{
-				newPosX = client.currentX
-			}
-			if rastojanje(p.posY, newPosY) < sizeof_client + sizeof_p{
-				newPosY = client.currentY
-			}
-		}
-
-		return (newPosX, newPosY)
-	}
-*/
 type PositionUpdateServer struct {
 	Id   string  `json:"id"`
 	PosX float64 `json:"x"`
